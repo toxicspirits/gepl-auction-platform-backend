bea035c20920bb7e9d58135c0071c2684655acf0
Apoorva Bordoloi <apoorva@tradexa.co.in>
2025-02-22 21:13:44 +0530

    hotfixes.

--- gepl_auction_platform_backend/users/api/serializers.py
+++ gepl_auction_platform_backend/users/api/serializers.py
@@ -6,7 +6,7 @@ from gepl_auction_platform_backend.users.models import User
 class UserSerializer(serializers.ModelSerializer[User]):
     class Meta:
         model = User
-        fields = ["username", "name", "user_type"]
+        fields = ["id", "username", "name", "user_type"]
 
         extra_kwargs = {
             "url": {"view_name": "api:user-detail", "lookup_field": "username"},
