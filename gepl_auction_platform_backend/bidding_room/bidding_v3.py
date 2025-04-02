e8d504563f94f62d058337c0b6faf2453a999ff3
Apoorva Bordoloi <apoorva@tradexa.co.in>
2025-04-02 15:57:36 +0530

    hotfixes.

--- gepl_auction_platform_backend/bidding_room/bidding_v3.py
+++ gepl_auction_platform_backend/bidding_room/bidding_v3.py
@@ -251,13 +251,13 @@ class AuctionConsumer(AsyncWebsocketConsumer):
             highest_bid = self.channel_layer.highest_bid
             current_budget = await Teams.objects.aget(owner=bidder)
             current_budget = current_budget.budget
-            override_vals = data.get("override_vals", False)
+            from_client = data.get("override_vals", True)
 
             if (
                 current_player
                 and current_player.get("name")
                 and current_budget >= bid_amount > self.channel_layer.last_bid
-                and override_vals
+                and from_client is False
             ):
                 bid_increase = bid_amount - (
                     highest_bid["bid_amount"] if highest_bid else 0
